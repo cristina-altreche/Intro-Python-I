72d3adefc8c82afe4be988f1ffac022ee86deb11
Cristina Altreche <c.altreche@gmail.com>
2020-08-31 11:32:02 -0400

    3-6

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -35,10 +35,19 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 # Write a function `print_tuple` that prints all the values in a tuple
 
 # YOUR CODE HERE
+def print_tuple(t):
+    for i in t:
+        print (i)
 
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
 # Declare a tuple of 1 element then print it
-u = (1)  # What needs to be added to make this work?
+def print_tuple(u):
+    for elem in u:
+        print(elem)
+
+u = (1,)  # What needs to be added to make this work?
 print_tuple(u)
+
+# Answering your question, (1) is just integer 1 with a grouping parenthesis. In order to recreate the singleton tuple via its representation, it has to be printed as (1,) which is the valid syntax for creating the tuple.
